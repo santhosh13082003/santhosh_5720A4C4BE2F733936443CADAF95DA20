c5a35360fc8676d69be73284468c0a68f455cc01
santhosh13082003 <144551006+santhosh13082003@users.noreply.github.com>
2023-09-16 21:08:56 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-def fact_res(n):
-      if n == 0 or n == 1:
-        return 1
-      else:
-        return n * fact_res(n - 1)
-
-number = int(input("Enter the value:"))
-res = fact_res(number)
-print("The factorial of {} is {}.".format(number, res))
